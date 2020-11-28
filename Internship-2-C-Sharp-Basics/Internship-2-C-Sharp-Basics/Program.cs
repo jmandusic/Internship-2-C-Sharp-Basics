71f60982c971e4d0a1aa882e37558c2f4e0a27f9
jmandusic <jere.mandusic2002@gmail.com>
2020-11-28 03:02:05 +0100

    Added options 6 and 9 to project and also possibility to go back to menu

--- Internship-2-C-Sharp-Basics/Internship-2-C-Sharp-Basics/Program.cs
+++ Internship-2-C-Sharp-Basics/Internship-2-C-Sharp-Basics/Program.cs
@@ -124,28 +124,58 @@ namespace Internship_2_C_Sharp_Basics
                             Console.WriteLine("Unesite ime nove pjesme");
                             var newSong = Console.ReadLine();
                             var lastSong = 0;
+                            var addNewSong = true;
                             foreach (var song in dictionary)
                             {
                                 if (song.Key > lastSong)
                                 {
                                     lastSong = song.Key;
                                 }
+                                if (song.Value == newSong)
+                                {
+                                    addNewSong = false;
+                                }
                             }
-                            dictionary.Add(lastSong + 1, newSong);
+                            if (addNewSong)
+                            {
+                               dictionary.Add(lastSong + 1, newSong);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ova pjesma se ne mmože dodati jer već postoji na playlisti.");
+                            }
                             break;
                         case 5:
                             Console.WriteLine("Izabrali ste brisanje pjesme unosom rednog broja pjesme");
                             var deleteSongDefinied = false;
-                            Console.WriteLine("Unesite broj pjesme koju želite izbrisati");
+                            Console.WriteLine("Ako niste sigurni u odluci, za povratak na izbornik unesite: izbornik");
+                            Console.WriteLine("Za nastavak unijeti bilo što");
+                            var backToMenu = Console.ReadLine();
+                            if (backToMenu == "izbornik")
+                            {
+                                Console.WriteLine();
+                                deleteSongDefinied = true;
+                                runProgram = false;
+                                runMenu = true;
+                            }
                             var listOfKeys5 = dictionary.Keys.ToList();
                             var listOfValues5 = dictionary.Values.ToList();
-                            dictionary.Clear();
                             while (deleteSongDefinied == false)
                             {
+                                dictionary.Clear();
+                                Console.WriteLine("Unesite broj pjesme koju želite izbrisati");
                                 var deleteSongNumber = int.Parse(Console.ReadLine());
                                 for (var i = 0; i < listOfKeys5.Count; i++)
                                 {
-                                    if (listOfKeys5[i] == deleteSongNumber)
+                                    if (deleteSongNumber == listOfKeys5[listOfKeys5.Count - 1])
+                                    {
+                                        listOfKeys5.Remove(listOfKeys5[listOfKeys5.Count - 1]);
+                                        listOfValues5.Remove(listOfValues5[listOfValues5.Count - 1]);
+                                        Console.WriteLine("Pjesma uspješno izbrisana!");
+                                        deleteSongNumber = 999;
+                                        deleteSongDefinied = true;
+                                    }
+                                    else if (listOfKeys5[i] == deleteSongNumber)
                                     {
                                         listOfKeys5.Remove(listOfKeys5[i]);
                                         listOfValues5.Remove(listOfValues5[i]);
@@ -162,10 +192,64 @@ namespace Internship_2_C_Sharp_Basics
                                 {
                                     Console.WriteLine("Ne postoji pjesma pod tim brojem. Molim ponovite unos!");
                                 }
+                                for (var i = 0; i < listOfKeys5.Count; i++)
+                                {
+                                    dictionary.Add(listOfKeys5[i], listOfValues5[i]);
+                                }
                             }
-                            for (var i = 0; i < listOfKeys5.Count; i++)
+                            break;
+                        case 6:
+                            Console.WriteLine("Izabrali ste brisanje pjesme unosom imena pjesme");
+                            var deleteSongByNameDefinied = false;
+                            Console.WriteLine("Ako niste sigurni u odluci, za povratak na izbornik unesite: izbornik");
+                            Console.WriteLine("Za nastavak unijeti bilo što");
+                            backToMenu = Console.ReadLine();
+                            if (backToMenu == "izbornik")
                             {
-                                dictionary.Add(listOfKeys5[i], listOfValues5[i]);
+                                Console.WriteLine();
+                                deleteSongByNameDefinied = true;
+                                runProgram = false;
+                                runMenu = true;
+                            }
+                            var listOfKeys6 = dictionary.Keys.ToList();
+                            var listOfValues6 = dictionary.Values.ToList();
+                            while (deleteSongByNameDefinied == false)
+                            {
+                                dictionary.Clear();
+                                Console.WriteLine("Unesite ime pjesme koju želite izbrisati");
+                                var deleteSong = Console.ReadLine();
+                                var deletedSongNumber = 999;
+                                for (var i = 0; i < listOfKeys6.Count; i++)
+                                {
+                                    if (deleteSong == listOfValues6[listOfValues6.Count - 1])
+                                    {
+                                        listOfKeys6.Remove(listOfKeys6[listOfKeys6.Count - 1]);
+                                        listOfValues6.Remove(listOfValues6[listOfValues6.Count - 1]);
+                                        Console.WriteLine("Pjesma uspješno izbrisana!");
+                                        deleteSongByNameDefinied = true;
+                                    }
+                                    else if (listOfValues6[i] == deleteSong)
+                                    {
+                                        deletedSongNumber = listOfKeys6[i];
+                                        listOfKeys6.Remove(listOfKeys6[i]);
+                                        listOfValues6.Remove(listOfValues6[i]);
+                                        Console.WriteLine("Pjesma uspješno izbrisana!");
+                                        listOfKeys6[i] = listOfKeys6[i] - 1;
+                                        deleteSongByNameDefinied = true;
+                                    }
+                                    else if (listOfKeys6[i] > deletedSongNumber)
+                                    {
+                                        listOfKeys6[i] = listOfKeys6[i] - 1;
+                                    }
+                                }
+                                if (deleteSongByNameDefinied == false)
+                                {
+                                    Console.WriteLine("Ne postoji pjesma pod tim imenom. Molim ponovite unos!");
+                                }
+                                for (var i = 0; i < listOfKeys6.Count; i++)
+                                {
+                                    dictionary.Add(listOfKeys6[i], listOfValues6[i]);
+                                }
                             }
                             break;
                         case 7:
@@ -194,13 +278,23 @@ namespace Internship_2_C_Sharp_Basics
                             break;
                         case 8:
                             Console.WriteLine("Izabrali ste uređivanje imena pjesme");
-                            Console.WriteLine("Unesite redni broj pjesme čije ime želite urediti");
                             var songNumberDefinied = false;
+                            Console.WriteLine("Ako niste sigurni u odluci, za povratak na izbornik unesite: izbornik");
+                            Console.WriteLine("Za nastavak unijeti bilo što");
+                            backToMenu = Console.ReadLine();
+                            if (backToMenu == "izbornik")
+                            {
+                                Console.WriteLine();
+                                songNumberDefinied = true;
+                                runProgram = false;
+                                runMenu = true;
+                            }
                             var listOfKeys8 = dictionary.Keys.ToList();
                             var listOfValues8 = dictionary.Values.ToList();
-                            dictionary.Clear();
                             while (songNumberDefinied == false)
                             {
+                                dictionary.Clear();
+                                Console.WriteLine("Unesite redni broj pjesme čije ime želite urediti");
                                 var songNumber = int.Parse(Console.ReadLine());
                                 for (var i = 0; i < listOfKeys8.Count; i++)
                                 {
@@ -217,13 +311,65 @@ namespace Internship_2_C_Sharp_Basics
                                 {
                                     Console.WriteLine("Ne postoji pjesma pod tim brojem. Molim ponovite unos!");
                                 }
-                            }
-                            for (var i = 0; i < listOfKeys8.Count; i++)
-                            {
-                                dictionary.Add(listOfKeys8[i], listOfValues8[i]);
+                                for (var i = 0; i < listOfKeys8.Count; i++)
+                                {
+                                    dictionary.Add(listOfKeys8[i], listOfValues8[i]);
+                                }
                             }
                             break;
+                        case 9:
+                            var listOfKeys9 = dictionary.Keys.ToList();
+                            var listOfValues9 = dictionary.Values.ToList();
+                            var listOfNewValues = new List<string>();
+                            Console.WriteLine("Izabrali ste premještanje pjesama");
+                            var songPositionDefinied = false;
+                            Console.WriteLine("Ako niste sigurni u odluci, za povratak na izbornik unesite: izbornik");
+                            Console.WriteLine("Za nastavak unijeti bilo što");
+                            backToMenu = Console.ReadLine();
+                            if (backToMenu == "izbornik")
+                            {
+                                Console.WriteLine();
+                                songPositionDefinied = true;
+                                runProgram = false;
+                                runMenu = true;
+                            }
+                            while (songPositionDefinied == false)
+                            {
+                                dictionary.Clear();
+                                Console.WriteLine("Unesite broj pjesme koju želite premjestiti");
+                                var songNumberPos = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Na koje mjesto ju želite premjestiti");
+                                var newSongPosition = int.Parse(Console.ReadLine());
+                                if (newSongPosition <= listOfKeys9.Count & songNumberPos <= listOfKeys9.Count)
+                                {
+                                    newSongPosition = newSongPosition - 1;
+                                    songNumberPos = songNumberPos - 1;
+                                    songPositionDefinied = true;
+                                    for (var i = 0; i < listOfValues9.Count; i++)
+                                    {
+                                        listOfNewValues.Add("*");
+                                    }
+                                    listOfNewValues[newSongPosition] = listOfValues9[songNumberPos];
+                                    listOfValues9.Remove(listOfValues9[songNumberPos]);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Molim ponovite unose");
+                                }
+                                for (var i = 0; i < listOfNewValues.Count; i++)
+                                {
+                                    if (listOfNewValues[i] == "*")
+                                    {
+                                        listOfNewValues[i] = listOfValues9[0];
+                                        listOfValues9.Remove(listOfValues9[0]);
+                                    }
+                                    dictionary.Add(listOfKeys9[i], listOfNewValues[i]);
+                                }
+                            }
+                           
+                            break;
                         default:
+                            Console.WriteLine("Molim ponovite unos");
                             break;
                     }
                     runProgramTime += 1;
